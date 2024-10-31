772d224cd79fdf965430064ab351289751cf139d
Sourny <brahim.sourny@gmail.com>
2024-10-31 20:59:23 +0100

    test(stories): fix headings and more

--- shared-ui/src/lib/Headings/index.stories.tsx
+++ shared-ui/src/lib/Headings/index.stories.tsx
@@ -10,20 +10,61 @@ const meta: Meta<typeof Heading> = {
 export default meta;
 type Story = StoryObj<typeof Heading>;
 
-export const Primary = {
-	args: {
-		as: '',
-		children: '',
-		style: '',
-		color: '',
-		role: '',
-		props: '',
-	},
-};
-
 export const H1: Story = {
 	args: {
 		as: 'h1',
+		children: 'Welcome to Heading!',
+		style: { fontFamily: 'sans-serif' },
+		color: 'red',
+		role: 'heading',
+	},
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		expect(canvas.getByText(/Welcome to Heading!/gi)).toBeTruthy();
+	},
+};
+export const H2: Story = {
+	args: {
+		as: 'h2',
+		children: 'Welcome to Heading!',
+		style: { fontFamily: 'sans-serif' },
+		color: 'red',
+		role: 'heading',
+	},
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		expect(canvas.getByText(/Welcome to Heading!/gi)).toBeTruthy();
+	},
+};
+export const H3: Story = {
+	args: {
+		as: 'h3',
+		children: 'Welcome to Heading!',
+		style: { fontFamily: 'sans-serif' },
+		color: 'red',
+		role: 'heading',
+	},
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		expect(canvas.getByText(/Welcome to Heading!/gi)).toBeTruthy();
+	},
+};
+export const H4: Story = {
+	args: {
+		as: 'h4',
+		children: 'Welcome to Heading!',
+		style: { fontFamily: 'sans-serif' },
+		color: 'red',
+		role: 'heading',
+	},
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		expect(canvas.getByText(/Welcome to Heading!/gi)).toBeTruthy();
+	},
+};
+export const H5: Story = {
+	args: {
+		as: 'h5',
 		children: 'Welcome to Heading!',
 		style: { fontFamily: 'sans-serif' },
 		color: 'red',
